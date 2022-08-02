d9f06ea883607a6573202d29df2f02c852f8c544
Teddy Muhammad Ripai <35595156+teddymr13@users.noreply.github.com>
2022-08-02 08:02:07 +0700

    add middleware and token jwt

--- app/app.go
+++ app/app.go
@@ -4,10 +4,14 @@ import (
 	"capi/domain"
 	"capi/logger"
 	"capi/service"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
@@ -65,14 +69,19 @@ func Start() {
 	// * create ServeMux
 	mux := mux.NewRouter()
 
+	authR := mux.PathPrefix("/auth").Subrouter()
+	authR.HandleFunc("/login", authH.Login).Methods(http.MethodPost)
+
+	authR.Use(loggingMiddleware)
 	// * defining routes
-	mux.HandleFunc("/auth/login", authH.Login).Methods(http.MethodPost)
+	// mux.HandleFunc("/auth/login", authH.Login).Methods(http.MethodPost)
 
 	mux.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	mux.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomerByID).Methods(http.MethodGet)
 	mux.HandleFunc("/customers/{customer_id:[0-9]+}/accounts", ah.NewAccount).Methods(http.MethodPost)
 	mux.HandleFunc("/customers/{customer_id:[0-9]+}/accounts/{account_id:[0-9]+}", ah.MakeTransaction).Methods(http.MethodPost)
 
+	mux.Use(authMiddleware)
 	// * starting the server
 
 	serverAddr := os.Getenv("SERVER_ADDRESS")
@@ -97,4 +106,46 @@ func getClientDB() *sqlx.DB {
 	logger.Info("success connect to database...")
 
 	return db
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		timer := time.Now()
+		next.ServeHTTP(w, r)
+		logger.Info(fmt.Sprintf("%v %v %v", r.Method, r.URL, time.Since(timer)))
+	})
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenHeader := r.Header.Get("Authorization")
+
+		// statment kondisi untuk token
+		if tokenHeader == "" {
+			// logger.Info("connection authorization token failed")
+			writeResponse(w, http.StatusUnauthorized, "connection authorization token failed")
+		}
+		//split token -> ambil tokennya buang "Bearer" nya
+		splitToken := strings.Split(tokenHeader, " ")
+		token := splitToken[1]
+		//parsing token, err := jwt.Parse(
+		parseToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+			return []byte("rahasia"), nil
+		})
+		//check token validation
+		if parseToken.Valid {
+			fmt.Println("token process successfully")
+		} else if errors.Is(err, jwt.ErrTokenMalformed) {
+			fmt.Println("That's not even a token")
+		} else if errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet) {
+			// Token is either expired or not active yet
+			fmt.Println("Timing is everything")
+		} else {
+			fmt.Println("Couldn't handle this token:", err)
+		}
+
+		logger.Info(token)
+
+		next.ServeHTTP(w, r)
+	})
 }
